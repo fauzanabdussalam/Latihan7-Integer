577fb03940e03820020c11ea641a6b88c61620dd
fauzan_abdussalam <fauzan@3trust.com>
2020-11-25 23:02:26 +0700

    Initial Commit

---
+++ src/pbo10k/pkg10119901/latihan7/integer/PBO10K10119901Latihan7Integer.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k.pkg10119901.latihan7.integer;
+
+/**
+ *
+ * @author
+ * NAMA     : Fauzan Muhammad Abdussalam
+ * KELAS    : IF-10K
+ * NIM      : 10119901
+ * Deskripsi Program : Menampilkan Program Integer
+ *
+ */
+public class PBO10K10119901Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 900000000000L;
+        
+        System.out.println("byte \t : " + b);
+        System.out.println("short \t : " + s);
+        System.out.println("int \t : " + i);
+        System.out.println("long \t : " + l);
+    }
+    
+}
